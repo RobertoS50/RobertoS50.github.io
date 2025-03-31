e7a85c178f8cd32908c71188ba1223c43227b845
alexrojas431 <alexrojas431@gmail.com>
2025-03-30 21:43:52 -0500

    Started Next.js app
    With following tech:
    - TypeScript
    - SCSS
    - Will likely use Jest again for it's unecesary right now
    
    - Added Roberto's picture
    - Made basic structure of website
    
    Features to do list:
    - Check which font to use TODO #1
    - Want to implement paralax scrolling TODO #2
    - Also want picture to be behind the content of the website TODO #3
    - Add easy navigation, like a button to get to certain part of website TODO #4
    
    Other todo list:
    - Clean up SCSS files and strip down to useful portions TODO #5
    - Check what can be taken from given CSS files TODO #6

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,94 +1,60 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import ProfilePic from "@/public/RobertoS-4.jpg";
+import styles from "@/scss/pages/home.module.scss";
+// TODO #2: Implement paralax scrolling
+// TODO #3: Have picture be behind main content of website
+// TODO #4: Add easy navigation like a button to get to certain parts of website
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      <div>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src={ProfilePic}
+          alt="A picture of myself - Roberto Soto"
+          width={1334}
+          height={1959}
           priority
+          style={{ width: "auto", height: "150px" }}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+      </div>
+      <header className={styles.header}>
+        <h1>Roberto Soto</h1>
+      </header>
+      <main className={styles.main}>
+        <section>
+          <article>
+            <h2>Bio</h2>
+            <p>
+              Hi My name is Roberto Soto I am currently a rising senior at
+              Aurora University. My passion is in Marketing and I have done
+              Brand Ambassador work since 2019. Working for brands such as
+              Gatorade, Coke, Insta Kart, and, most recently for 2024,
+              LollaPalooza and American Apparel. I&apos;ve also managed a local
+              company&apos;s social media handles as well. Besides focusing on
+              my career I like to go to concerts in my free time as well as
+              acting as a hobby.
+            </p>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h3>Background</h3>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h4>Work experience</h4>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h5>More room</h5>
+          </article>
+        </section>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        Website coded by Alexander Rojas
       </footer>
     </div>
   );
