25a5a84544f6785df5c3a40dc1949b4e2645427b
alexrojas431 <alexrojas431@gmail.com>
2025-04-01 19:56:56 -0500

    Put picture behind content of website, implemented parallax scrolling TODO #3, TODO #2
    home page.tsx and home css
    - Added classNames to relevant tags
    - Added image wrapper and content wrapper for use in parallax scrolling
    
    home SCSS
    - Made SCSS work for mobile first
    - Took out unused SCSS
    
    May take out further unnecessary wrappers and SCSS

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -8,54 +8,58 @@ import styles from "@/scss/pages/home.module.scss";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <div>
-        <Image
-          src={ProfilePic}
-          alt="A picture of myself - Roberto Soto"
-          width={1334}
-          height={1959}
-          priority
-          style={{ width: "auto", height: "150px" }}
-        />
+      <section className={styles.parallaxSection}>
+        <div className={styles.parallaxContainer}>
+          <Image
+            src={ProfilePic}
+            alt="A picture of myself - Roberto Soto"
+            className={styles.parallaxImage}
+            sizes="100svw"
+            fill
+            priority
+          />
+        </div>
+      </section>
+      <div className={styles.contentOverlay}>
+        <header className={styles.header}>
+          <h1>Roberto Soto</h1>
+        </header>
+        <main className={styles.main}>
+          <section>
+            <article>
+              <h2>Bio</h2>
+              <p>
+                Hi My name is Roberto Soto I am currently a rising senior at
+                Aurora University. My passion is in Marketing and I have done
+                Brand Ambassador work since 2019. Working for brands such as
+                Gatorade, Coke, Insta Kart, and, most recently for 2024,
+                LollaPalooza and American Apparel. I&apos;ve also managed a
+                local company&apos;s social media handles as well. Besides
+                focusing on my career I like to go to concerts in my free time
+                as well as acting as a hobby.
+              </p>
+            </article>
+          </section>
+          <section>
+            <article>
+              <h3>Background</h3>
+            </article>
+          </section>
+          <section>
+            <article>
+              <h4>Work experience</h4>
+            </article>
+          </section>
+          <section>
+            <article>
+              <h5>More room</h5>
+            </article>
+          </section>
+        </main>
+        <footer className={styles.footer}>
+          Website coded by Alexander Rojas
+        </footer>
       </div>
-      <header className={styles.header}>
-        <h1>Roberto Soto</h1>
-      </header>
-      <main className={styles.main}>
-        <section>
-          <article>
-            <h2>Bio</h2>
-            <p>
-              Hi My name is Roberto Soto I am currently a rising senior at
-              Aurora University. My passion is in Marketing and I have done
-              Brand Ambassador work since 2019. Working for brands such as
-              Gatorade, Coke, Insta Kart, and, most recently for 2024,
-              LollaPalooza and American Apparel. I&apos;ve also managed a local
-              company&apos;s social media handles as well. Besides focusing on
-              my career I like to go to concerts in my free time as well as
-              acting as a hobby.
-            </p>
-          </article>
-        </section>
-        <section>
-          <article>
-            <h3>Background</h3>
-          </article>
-        </section>
-        <section>
-          <article>
-            <h4>Work experience</h4>
-          </article>
-        </section>
-        <section>
-          <article>
-            <h5>More room</h5>
-          </article>
-        </section>
-      </main>
-      <footer className={styles.footer}>
-        Website coded by Alexander Rojas
-      </footer>
     </div>
   );
 }
