178acb89d0bdde500039bcf7919ff85968849f2d
alexrojas431 <alexrojas431@gmail.com>
2025-04-14 22:19:31 -0500

    Reword content. Changed rem to %. Resort to % more than rem.
    - rem changed to % as rem didn't scale down size within mobile views. 2 rem font size was 32px on mobile and desktop which was too large on mobile. This behavior may be desired on items that should be constant on both views but that's not the intention most of the time.
    
    Intro tsx
    - Add new lines and reword some content for sake of clarity
    
    intro scss
    - Removed unused list attribute
    - Changed value of "justify-content", so space is set between the items rather than at the ends of the flexbox
    
    root scss
    - Changed rem to %, in places where the difference was noticeable in mobile but not in desktop
    
    home scss
    - Changed rem to %, in places where the difference was noticeable in mobile but not in desktop

--- website/src/lib/components/sections/Intro.tsx
+++ website/src/lib/components/sections/Intro.tsx
@@ -5,19 +5,24 @@ export default function Intro() {
     <section className={styles.introSection}>
       <article className={styles.bio}>
         <p>
-          Hi My name is Roberto Soto I am currently a rising senior at Aurora
+          Hi, my name is Roberto Soto I am currently a rising senior at Aurora
           University. My passion is in Marketing and I have done Brand
-          Ambassador work since 2019. Working for brands such as Gatorade, Coke,
-          Insta Kart, and, most recently for 2024, LollaPalooza and American
-          Apparel. I&apos;ve also managed a local company&apos;s social media
-          handles as well. Besides focusing on my career I like to go to
-          concerts in my free time as well as acting as a hobby.
+          Ambassador work since 2019.
+          <br />
+          <br />
+          Working for brands such as Gatorade, Coke, Insta Kart, and, most
+          recently for 2024, LollaPalooza and American Apparel. I&apos;ve also
+          managed a local company&apos;s social media handles as well.
+          <br />
+          <br />
+          Besides focusing on my career I like to go to concerts in my free time
+          as well as acting as a hobby.
         </p>
       </article>
       <article className={styles.contactInfo}>
-        Contact me at:
+        Contact me through my:
         <address>
-          <a href="https://www.google.com">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/roberto-soto12/">LinkedIn</a>
           <br />
           <a href="mailto:sotor2588@gmail.com">Email - sotor2588@gmail.com</a>
         </address>
