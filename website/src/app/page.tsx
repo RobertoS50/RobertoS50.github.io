136cd4c8dee723ccd84d18d4319661f13239048a
alexrojas431 <alexrojas431@gmail.com>
2025-07-20 19:32:46 -0500

    Added animation on entry for main content section
    Removed completed TODO 37 and 41

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -6,13 +6,8 @@ import styles from "@/scss/pages/home.module.scss";
 import ProgressiveImage from "@/components/ProgressiveImage";
 
 // TODO #9/9: !!FUTURE!! Design components to be loaded in dynamically. Intersection observer API (This website can grow a lot if it stays one page)
-// TODO #37: Add animation when the user enters the page and even when the user scrolls down the page.
-// * (Entry) contentOverlay scrolls up. (Scroll down) content fades in
-// * On scroll, have parallax image fade or have the contentOverlay a shadow box at the top of the overlay.
-
 // TODO 39: Reduce main thread blocker
 // TODO 40: Reduce resource delay and element render delay
-// TODO 41: Stick to transform and opacity for animations (non-composite animations)
 // TODO 42: Look for the render blockers
 // TODO 44: html tree map and other SEO practices
 
